3b0d0a29be2d87785a9d3cf7b5aef2e01aa1f77e
Spiro94 <danieljosevillamizar@gmail.com>
2025-10-22 17:37:26 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AllPosts from './AllPosts'
+import './App.css'
+import MessageBoard from './MessageBoard'
+import NavBar from './NavBar'
+import PostView from './PostView'
+import Welcome from './Welcome'
+import { type SupashipUserInfo, useSession } from './use-session'
+import { createContext } from 'react'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <MessageBoard />,
+        children: [
+          {
+            path: ":pageNumber",
+            element: <AllPosts />,
+          },
+          {
+            path: "post/:postId",
+            element: <PostView />,
+          },
+        ],
+      },
+      {
+        path: "welcome",
+        element: <Welcome />,
+        // loader: welcomeLoader,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
+export const UserContext = createContext<SupashipUserInfo>({
+  session: null,
+  profile: null,
+});
+
+function Layout() {
+  const supashipUserInfo = useSession();
+  return (
+    <>
+      <UserContext.Provider value={supashipUserInfo}>
+        <NavBar />
+        <Outlet />
+      </UserContext.Provider>
+    </>
+  );
+}
---
+++ src/supa-client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_API_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const supaClient = createClient(
+    supabaseUrl,
+    supabaseAnonKey,
+);
